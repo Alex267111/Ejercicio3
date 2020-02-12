bd922c397c5a50f99457276ad35fdf16b9aa407d
GFernandoCT <58846148+GFernandoCT@users.noreply.github.com>
2020-02-12 19:58:18 +0100

    Add files via upload

---
+++ IFactura.java
@@ -0,0 +1,18 @@
+public class IFactura {
+
+    public IFactura() {
+    }
+
+    public void cambiarEstado(Estado estado) {
+
+    }
+
+    public float getTotal() {
+
+    }
+
+    public float getCantidadIva() {
+
+    }
+
+}
