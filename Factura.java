d5883fd08599280a8f9f39851a388fd22e78c7a2
Alex <58846498+Alex267111@users.noreply.github.com>
2020-02-12 21:00:21 +0100

    Create Factura.java

---
+++ Factura.java
@@ -0,0 +1,24 @@
+/**
+ * Clase definida como elemento basico a la hora de crear facturas para el programa
+ *
+ * @author Alexander Correa
+ */
+
+public class Factura{
+  public int num; 
+  public float base; 
+  public float tipoIva; 
+  public Estados estado; 
+  /**
+   * Constructor de la clase factura.
+   * 
+   * @param num 
+   * @param base 
+   * @param tipoIva 
+   * @param estado 
+   */
+  
+  public static void main(String[] args) {
+    
+  }
+}
