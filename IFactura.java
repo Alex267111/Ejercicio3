0c92cbf59f12000ba4cdb87ea17445369bf3a071
GFernandoCT <58846148+GFernandoCT@users.noreply.github.com>
2020-02-12 21:33:48 +0100

    Update IFactura.java

--- IFactura.java
+++ IFactura.java
@@ -1,18 +1,24 @@
+/** La clase IFactura es una interfaz para la Factura
+*   
+*   @FernandoCT.
+*/
+
 public class IFactura {
 
     public IFactura() {
     }
-
-    public void cambiarEstado(Estado estado) {
-
+    
+    /** Cambia el estado de factura con el tipoEnum Estados
+    * @param estado estado de la factura
+    */
+    
+    public void cambiarEstado(Estado estado) {                          
     }
-
+                                                                        
     public float getTotal() {
-
     }
 
     public float getCantidadIva() {
-
     }
 
 }
