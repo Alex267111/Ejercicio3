a2af3cb5ffb7b1e95ef1ff0eedf4dbd89d6d4c1b
Paul Minaya <paul.minaya1k@gmail.com>
2020-02-14 12:24:31 +0100

    Update Factura.java

--- Factura.java
+++ Factura.java
@@ -1,26 +1,26 @@
 /**
  * Clase definida como elemento basico a la hora de crear facturas para el programa
  *
- * @author Alexander Correa, Fernando Correa y Paul 
+ * @author Alexander Correa, Fernando Correa y Paul
  */
 
 public class Factura{
-  public int num; 
-  public float base; 
-  public float tipoIva; 
-  protected Estados estado; 
+  public int num;
+  public float base;
+  public float tipoIva;
+  protected Estados estado;
 
   /**
    * Constructor de la clase factura.
-   * 
-   * @param num 
-   * @param base 
+   *
+   * @param num
+   * @param base
    * @param tipoIva El tipo del iva aplicado a la factura
    * @param estado Estado en el que se encuentra la factura (enum)
    */
-  
+
   public static void main(String[] args) {
-    
+
   }
 
   public void cambiarEstado(Estado estado) {
@@ -28,17 +28,19 @@ public class Factura{
 
   /**
    * Devuelve un float que indica la cantidad total de la factura
-   * 
+   *
    * @return float que indica el precio total de la factura
   **/
   public float getTotal() {
+    return total;
   }
-  
+
   /**
    * Devuelve un float que indica la cantidad de IVA aplicado a la factura
-   * 
+   *
    * @return float que indica el IVA de la factura
-  **/ 
+  **/
   public float getCantidadIva() {
+    return iva;
   }
 }
